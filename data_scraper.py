307b1a99c7c78e34136b2ed5f29fb82c72921521
NeelOommen <neelittyerahoommen@gmail.com>
2024-01-29 16:50:32 +0530

    Initial Code Commit
    Data Scraper Script with character separation

---
+++ data_scraper.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from utility import collectArea
+
+file_name = 's1.jpg'
+
+img = cv2.imread('src_images\\' + file_name)
+img_demo = cv2.imread('src_images\\' + file_name)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#img_demo = cv2.cvtColor(img_demo, cv2.COLOR_BGR2GRAY)
+img = cv2.bitwise_not(img)
+img = cv2.threshold(img, 85, 255, cv2.THRESH_BINARY)[1]
+
+height, width = img.shape
+
+print(f'{width} {height}')
+
+#apply blur
+blur_factor = 7
+kernel = np.ones((blur_factor, blur_factor), np.float32)/(blur_factor*blur_factor)
+img = cv2.filter2D(img, -1, kernel=kernel)
+#img = cv2.blur(img, (bl ur_factor, blur_factor))
+
+#crush white
+# for i in range(0, width, 1):
+#     for j in range(0, height, 1):
+#          if img[j][i] != 0:
+#             img[j][i] = 255
+
+for i in range(0 , width, 1):
+    for j in range(0 , height, 1):
+         if img[j][i] > 0:
+            left,top,right,bottom = collectArea(img, i, j)
+            cv2.rectangle(img_demo, (top,left), (bottom,right), (0,0,255), 1)
+            
+#debug
+f = open('dump.txt', 'w')
+for i in range(0 , width, 1):
+    for j in range(0 , height, 1):
+        f.write(str(img[j][i]) + '\t')
+    f.write('\n')
+f.close()
+
+
+cv2.imshow('test', img_demo)
+#cv2.imshow('test2', img)
+cv2.waitKey(0)
---
+++ utility.py
@@ -0,0 +1,41 @@
+def collectArea(img, i, j):
+    leftBound = j
+    rightBound = j
+    topBound = i
+    bottomBound = i
+
+    to_explore = []
+    visited = []
+
+    start_pixel = (i,j)
+    to_explore.append(start_pixel)
+
+    while len(to_explore) != 0 :
+        currentI, currentJ = to_explore.pop(0)
+        img[currentJ][currentI] = 0
+        visited.append((currentI, currentJ))
+
+        #add non zero neightbours
+        tI = currentI-1
+        while tI <= currentI+1:
+            tJ = currentJ-1
+            while tJ <= currentJ+1:
+                if img[tJ][tI] > 0 and tJ != currentJ and tI != currentI:
+                    newPixel = (tI, tJ)
+                    img[tJ][tI] = 0
+                    if not (newPixel in to_explore) and not (newPixel in visited):
+                        to_explore.append(newPixel)
+                tJ+=1
+            tI+=1 
+        #print(s)
+
+        #update limits
+        leftBound = min(currentJ, leftBound)
+        rightBound = max(currentJ, rightBound)
+        topBound = min(currentI, topBound)
+        bottomBound = max(currentI, bottomBound)
+        
+
+
+    print(f'bounds: {leftBound} {topBound} {rightBound} {bottomBound}')
+    return (leftBound, topBound, rightBound, bottomBound)
