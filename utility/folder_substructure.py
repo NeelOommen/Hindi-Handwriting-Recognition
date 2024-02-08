de0c53f796c673df8b8ab729c5a4209e435c4ec3
NeelOommen <neelittyerahoommen@gmail.com>
2024-02-08 13:26:20 +0530

    Emptied repository
    Emptied repo to make sure the gitignore works

--- utility/folder_substructure.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from unicode_list import hindi_unicode_characters
-
-for i in hindi_unicode_characters:
-    os.mkdir(os.path.join('..\Hindi-Handwriting-Recognition\Dataset', i))
-
-print('Folders Created')
