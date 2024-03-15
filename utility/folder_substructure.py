fb0de005e3fd2d8e25b1fac9de1a1a8a60774bcd
NeelOommen <neelittyerahoommen@gmail.com>
2024-03-15 13:39:28 +0530

    temp dataset

--- utility/folder_substructure.py
+++ utility/folder_substructure.py
@@ -3,6 +3,7 @@ import os
 from unicode_list import hindi_unicode_characters
 
 for i in hindi_unicode_characters:
-    os.mkdir(os.path.join('..\Hindi-Handwriting-Recognition\Dataset', i))
+    #os.mkdir(os.path.join('..\Hindi-Handwriting-Recognition\Dataset', i))
+    print(i)
 
 print('Folders Created')
